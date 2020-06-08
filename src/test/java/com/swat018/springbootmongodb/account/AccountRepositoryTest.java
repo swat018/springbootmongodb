d4a6fac5e6d478df222c354a3221ef8f0f04bdd6
jinwoo park <swat018@gmail.com>
2020-06-09 04:27:53 +0900

    스프링 데이터 - MongoDB

--- src/test/java/com/swat018/springbootmongodb/account/AccountRepositoryTest.java
+++ src/test/java/com/swat018/springbootmongodb/account/AccountRepositoryTest.java
@@ -1,5 +1,8 @@
 package com.swat018.springbootmongodb.account;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +10,11 @@ import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.OPTIONAL;
+
 import java.util.Optional;
 
+@Data
 @RunWith(SpringRunner.class)
 @DataMongoTest
 public class AccountRepositoryTest {
@@ -19,19 +25,17 @@ public class AccountRepositoryTest {
     @Test
     public void findByEmail() {
         Account account = new Account();
-        account.setUsername("jinwoo");
-        account.setEmail("swat018@gmail.com");
+        account.setEmail("abv@gmail.com");
+        account.setUsername("keesun");
 
         accountRepository.save(account);
 
         Optional<Account> byId = accountRepository.findById(account.getId());
         assertThat(byId).isNotEmpty();
-/*
+
         Optional<Account> byEmail = accountRepository.findByEmail(account.getEmail());
         assertThat(byEmail).isNotEmpty();
-        assertThat(byEmail.get().getUsername()).isEqualTo("jinwoo");
-*/
-
+        assertThat(byEmail.get().getUsername()).isEqualTo("keesun");
     }
 }
 
